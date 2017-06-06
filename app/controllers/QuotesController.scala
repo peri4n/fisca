cbd368b6bb91ab2b4c428a7980ce6037f793f390
Fabian Bull <fabian.bull@bioinform.at>
2017-06-06 20:40:39 +0200

    Add my first asynchronous controller

--- app/controllers/QuotesController.scala
+++ app/controllers/QuotesController.scala
@@ -1,11 +1,21 @@
 package controllers
 
+import javax.inject.Inject
+
+import play.api.db.DBApi
+import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, Controller}
 
-class QuotesController extends Controller {
+import scala.concurrent.ExecutionContext.Implicits.global
 
-  def get(ticker: String, start: Option[String]) = Action {
-    Ok(ticker + " " + start)
+class QuotesController @Inject()(val db: DBApi, val ws: WSClient) extends Controller {
+
+  def get(ticker: String, start: Option[String]) = Action.async {
+    ws.url("http://www.google.com/finance/historical?cid=22144&startdate=Jun+1%2C+2015&enddate=Jun+6%2C+2017&num=30&output=csv")
+      .get()
+      .map { response =>
+        Ok(response.body)
+      }
   }
 
 }
--- app/controllers/TickerController.scala
+++ app/controllers/TickerController.scala
@@ -9,7 +9,7 @@ import play.api.mvc.{Action, Controller}
 class TickerController @Inject()(val db: DBApi) extends Controller {
 
   def list = Action {
-    db.database("default").withConnection { implicit c =>
+    db.database("default").withConnection { implicit connection =>
       val allNames = SQL(s"SELECT ticker FROM symbol;").as(SqlParser.str("ticker").*)
       Ok(allNames.mkString(","))
     }
